449594ef1d5a6eae992af826d62714a7badce271
Vivek Sharma <112774647+Vivek02Sharma@users.noreply.github.com>
2022-09-06 00:57:04 +0530

    Create Find your stage of life.py

---
+++ Find your stage of life.py
@@ -0,0 +1,37 @@
+#What does it mean to “live healthy”?
+
+# Depends on your age. From infancy all the way to senior adulthood, the components that contribute to a healthy life – things like nutrition, fitness, mental health and prevention – will look different.
+# For example, just as it is important for an older adult to get 20 minutes of moderate activity a day, it is key for infants to have daily “tummy time.” This blog series will be a guide to essential healthy living choices you can make for you and your family during each “Stage of Life,” with input from INTEGRIS physicians. We know most readers live in the area, so we plan to include Oklahoma-specific healthy living ideas whenever we can, for each “Stage of Life,” with guides to local areas for wellness and nutrition.
+
+#What are the Stages of Life?
+
+#Infant = 0-1 year.
+#Toddler = 2-4 yrs.
+#Child = 5-12 yrs.
+#Teen = 13-19 yrs.
+#Adult = 20-39 yrs.
+#Middle Age Adult = 40-59 yrs.
+#Senior Adult = 60+
+
+#--------------------------------------------#
+
+print("            # STAGEOFLIFE #")
+print("\nThroughout the human life cycleThe span of a human life, which consists of different stages, including childhood, adolescence, adulthood, and old age., the body constantly changes and goes through different periods known as stages.\n\n ")
+
+print("Your life stage is ",end=" ")
+#Input your year between 1 to 120.
+#Input your year between 1 to 120.
+x=int(input())
+if x<=1:print("Infant")
+elif x<=4:print("Toddler")
+elif x<=12:print("Child")
+elif x<=19:print("Teen")
+elif x<=39:print("Adult")
+elif x<=59:print("Middle Age Adult")
+elif x<=100:print("Senior Adult")
+elif x>=100:print("more then senior Adult")
+else:
+    print("Error")
+print("\n\nThanks for watching,please like MY PYTHON  CODE\n\n")
+print("©:copyright | 11 May 2021 | Privacy Policy | Created by Vivek Sharma R.")
+
