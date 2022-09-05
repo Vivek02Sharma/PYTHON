60d844660b2d02ecf5a1bbc32cbdd26d79321790
Vivek Sharma <112774647+Vivek02Sharma@users.noreply.github.com>
2022-09-06 00:53:58 +0530

    Create Find number of vowels.py

---
+++ Find number of vowels.py
@@ -0,0 +1,9 @@
+str=input()
+print("Your enter word or text :",str)
+count=0
+for v in str:
+   if v=="a" or v=="e" or v=="i" or v=="o"    or v=="u":
+    count+=1
+print("\nNumber of vowels :",count)
+
+print("\n\n\n©:copyright | 14 May 2022 | Privacy Policy | Created by VIVEK SHARMA R.")
